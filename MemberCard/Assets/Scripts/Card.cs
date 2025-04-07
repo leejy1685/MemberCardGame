2542cd7b1be069da4ea788dac3c9360d560ce321
jjmm-del <jimin8768@naver.com>
2025-04-07 20:20:27 +0900

    add:add only
    add file

---
+++ MemberCard/Assets/Scripts/Card.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int idx = 0;
+    public GameObject front;
+    public GameObject back;
+    public Animator anim;
+
+    public void OpenCard()
+    {
+        anim.SetBool("isOpen", true);
+        front.SetActive(true);
+        back.SetActive(false); 
+    }
+
+    public SpriteRenderer frontImage;
+
+    public void setting(int number)
+    {
+        idx = number;
+        frontImage.sprite = Resources.Load<Sprite>($"card{idx}");
+    }
+}
