1a13551ec9c44eaaf0457ea7f8fc66176637d583
yj6662 <yj6662@naver.com>
2025-04-07 19:23:51 +0900

    feat Card_Flip_Button
    -feat Card_Flip_Button

--- MemberCard/Assets/Scripts/Card.cs
+++ MemberCard/Assets/Scripts/Card.cs
@@ -6,6 +6,17 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
     public int idx = 0;
+    public GameObject front;
+    public GameObject back;
+    public Animator anim;
+
+    public void OpenCard()
+    {
+        Debug.Log("OpenCard »£√‚µ !");
+        anim.SetBool("isOpen", true);
+        front.SetActive(true);
+        back.SetActive(false); 
+    }
 
     public SpriteRenderer frontImage;
 
