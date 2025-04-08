7eb466fee6f64852ca618a1ee9ff0519f58e8ab0
Yamatchi <ussousso12@gmail.com>
2025-04-08 11:35:57 +0900

    docs : GameManager
    1. Gameover() 메서드 추가
    2. 시간제한 추가 (제한둘지 안둘지 추후 결정)
    3. scoreTxt 추가
    4. stageTxt 추가 (추후 stage생기면 수정)
       현재 stage = 1만 잡아놓음

--- MemberCard/Assets/Scripts/GameManager.cs
+++ MemberCard/Assets/Scripts/GameManager.cs
@@ -7,14 +7,22 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    // Card에서 1번 2번카드 idx를 넘겨받음
-    //public Card firstCard;
-    //public Card secondCard;
+    public Card firstCard;
+    public Card secondCard;
 
     public Text timeTxt;
+    public Text scoreTxt;
+    public Text stageTxt;
+    public GameObject endTxt;
+
     float time = 0.0f;
+    int score = 0;
+    int stage = 1;
+
+    public int cardCount = 0;
 
     AudioSource audioSource;
+
     void Awake()
     {
         if (Instance == null)
@@ -22,24 +30,56 @@ public class GameManager : MonoBehaviour
     }
     void Start()
     {
+        Time.timeScale = 1.0f;
         audioSource = GetComponent<AudioSource>();
     }
     void Update()
     {
-        time += Time.deltaTime;
+        // 시간 제한
+        if (time > 10.0f)
+        {
+            time = 30.0f;
+            Gameover();
+        }
+        else
+        {
+            time += Time.deltaTime;
+        }
+        // time += Time.deltaTime;
         timeTxt.text = time.ToString("N2");
     }
-    void cardMached()
+    public void cardMached()
     {
-        //if ()
-        //{
-        //    // 서로 idx가 일치하면 destroy
-        //    // Destroy();
-        //}
-        //else
-        //{
-        //    // 일치 하지 않으면 카드 뒤집어 놓음
+        if (firstCard.idx == secondCard.idx)
+        {
+            // idx가 일치하면 destroyCard
+            firstCard.DestroyCard();
+            secondCard.DestroyCard();
+            // Board에서 arr[i]값 받아오기
+            cardCount -= 2;
+            score++;
 
-        //}
+            if(cardCount == 0) // 모두 맞추면 게임 종료
+            {
+                Gameover();
+            }
+        }
+        else
+        {
+            // idx가 일치 하지 않으면 closeCard
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+        firstCard = null;
+        secondCard = null;
+    }
+
+    public void Gameover()
+    {
+        endTxt.SetActive(true);
+        Time.timeScale = 0f;
+
+        scoreTxt.text = score.ToString();
+        stageTxt.text = stage.ToString();
     }
 }
